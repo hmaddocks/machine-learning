96dab9fc021ef123acf109d7e1c5e2cc7e19dd3c
Henry Maddocks <hmaddocks@me.com>
2023-03-05 16:20:08 +1300

    Refactor to use ndarray

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::anyhow;
+use ndarray::{array, Array1};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -6,19 +7,19 @@ fn predict(input: f64, weight: f64, bias: f64) -> f64 {
     input * weight + bias
 }
 
-fn predict_vec(inputs: &[f64], weight: f64, bias: f64) -> Vec<f64> {
+fn predict_ndarray(inputs: &Array1<f64>, weight: f64, bias: f64) -> Array1<f64> {
     inputs
         .iter()
         .map(|&input| predict(input, weight, bias))
         .collect()
 }
 
-fn average(samples: &Vec<f64>) -> f64 {
+fn average(samples: &Array1<f64>) -> f64 {
     samples.iter().sum::<f64>() / (samples.len() as f64)
 }
 
-fn loss(inputs: &[f64], labels: &[f64], weight: f64, bias: f64) -> f64 {
-    let prediction = predict_vec(inputs, weight, bias);
+fn loss(inputs: &Array1<f64>, labels: &Array1<f64>, weight: f64, bias: f64) -> f64 {
+    let prediction = predict_ndarray(inputs, weight, bias);
     let error = prediction
         .iter()
         .zip(labels.iter())
@@ -28,10 +29,10 @@ fn loss(inputs: &[f64], labels: &[f64], weight: f64, bias: f64) -> f64 {
     average(&error)
 }
 
-fn gradient(inputs: &[f64], labels: &[f64], weight: f64, bias: f64) -> (f64, f64) {
-    let prediction = predict_vec(inputs, weight, bias);
+fn gradient(inputs: &Array1<f64>, labels: &Array1<f64>, weight: f64, bias: f64) -> (f64, f64) {
+    let prediction = predict_ndarray(inputs, weight, bias);
 
-    let errors: Vec<f64> = prediction
+    let errors: Array1<f64> = prediction
         .iter()
         .zip(labels.iter())
         .map(|(prediction, label)| prediction - label)
@@ -42,13 +43,18 @@ fn gradient(inputs: &[f64], labels: &[f64], weight: f64, bias: f64) -> (f64, f64
         .zip(inputs.iter())
         .map(|(error, input)| error * input)
         .collect();
-    let weight_gradient = average(&weight_gradient) * 2.0;
 
+    let weight_gradient = average(&weight_gradient) * 2.0;
     let bias_gradient = average(&errors) * 2.0;
     (weight_gradient, bias_gradient)
 }
 
-fn train(inputs: Vec<f64>, labels: Vec<f64>, iterations: usize, learning_rate: f64) -> (f64, f64) {
+fn train(
+    inputs: Array1<f64>,
+    labels: Array1<f64>,
+    iterations: usize,
+    learning_rate: f64,
+) -> (f64, f64) {
     let mut weight = 0.0;
     let mut bias = 0.0;
     for _ in 0..iterations {
@@ -60,11 +66,12 @@ fn train(inputs: Vec<f64>, labels: Vec<f64>, iterations: usize, learning_rate: f
     (weight, bias)
 }
 
-fn read_file(path: &str) -> anyhow::Result<(Vec<f64>, Vec<f64>)> {
+fn read_file(path: &str) -> anyhow::Result<(Array1<f64>, Array1<f64>)> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
-    let mut inputs = Vec::new();
-    let mut labels = Vec::new();
+
+    let mut col1 = Vec::new();
+    let mut col2 = Vec::new();
 
     for line in reader.lines() {
         let line = line?;
@@ -77,11 +84,14 @@ fn read_file(path: &str) -> anyhow::Result<(Vec<f64>, Vec<f64>)> {
         let num1 = parts[0].parse()?;
         let num2 = parts[1].parse()?;
 
-        inputs.push(num1);
-        labels.push(num2);
+        col1.push(num1);
+        col2.push(num2);
     }
 
-    Ok((inputs, labels))
+    let arr1 = Array1::from(col1);
+    let arr2 = Array1::from(col2);
+
+    Ok((arr1, arr2))
 }
 
 fn main() {
@@ -90,8 +100,10 @@ fn main() {
         Ok((inputs, labels)) => {
             let (weight, bias) = train(inputs, labels, 20000, 0.001);
             println!("weight: {:?}, bias: {:?}", weight, bias);
-            let prediction = predict(20f64, weight, bias);
-            println!("prediction: x = {:?}, y = {:?}", 20, prediction);
+
+            let target = 13.;
+            let prediction = predict(target, weight, bias);
+            println!("prediction: x = {:?}, y = {:?}", target, prediction);
         }
         Err(e) => {
             eprintln!("Error: {}", e);
@@ -110,14 +122,14 @@ mod tests {
     }
 
     #[test]
-    fn test_predict_vec() {
-        let (inputs, weight, bias) = (vec![20.0, 30.0], 1.5, 10.0);
-        assert_eq!(predict_vec(&inputs, weight, bias), vec![40.0, 55.0]);
+    fn test_predict_ndarray() {
+        let (inputs, weight, bias) = (array![20.0, 30.0], 1.5, 10.0);
+        assert_eq!(predict_ndarray(&inputs, weight, bias), array![40.0, 55.0]);
     }
 
     #[test]
     fn test_average() {
-        let collection = vec![25.0, 35.0, 60.0];
+        let collection = array![25.0, 35.0, 60.0];
         let average = average(&collection);
         assert_eq!(average, 40.0);
     }
